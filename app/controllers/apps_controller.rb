5942629ac673ec76f70d03cdac0049cdf48cc86e
John Rees <john@bitsushi.com>
2012-05-21 18:20:18 +0100

    started ransack

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,16 +1,19 @@
 class AppsController < ApplicationController
 
   def index
+    @q = controller_name.classify.constantize.search(params[:q])
     if current_user
-      @apps = controller_name.classify.constantize.text_search(params[:query])
-      .select("apps.*, CASE WHEN users.id = '#{current_user.id}' THEN 'true' ELSE 'false' END AS is_watched")
-      .joins('LEFT OUTER JOIN "lenses" ON "lenses"."app_id" = "apps"."id" LEFT OUTER JOIN "users" ON "users"."id" = "lenses"."watcher_id"')
-      .page params[:page]
+      @apps = @q.result(:distinct => true)
+      # @apps = .text_search(params[:query])
+      # .select("apps.*, CASE WHEN users.id = '#{current_user.id}' THEN 'true' ELSE 'false' END AS is_watched")
+      # .joins('LEFT OUTER JOIN "lenses" ON "lenses"."app_id" = "apps"."id" LEFT OUTER JOIN "users" ON "users"."id" = "lenses"."watcher_id"')
+      # .page params[:page]
     else
-      @apps = controller_name.classify.constantize.text_search(params[:query])
-      .select("apps.*, CASE WHEN users.id IS NOT NULL THEN 'false' ELSE 'false' END AS is_watched")
-      .joins('LEFT OUTER JOIN "lenses" ON "lenses"."app_id" = "apps"."id" LEFT OUTER JOIN "users" ON "users"."id" = "lenses"."watcher_id"')
-      .page params[:page]
+      @apps = @q.result(:distinct => true)
+      # @apps = controller_name.classify.constantize.text_search(params[:query])
+      # .select("apps.*, CASE WHEN users.id IS NOT NULL THEN 'false' ELSE 'false' END AS is_watched")
+      # .joins('LEFT OUTER JOIN "lenses" ON "lenses"."app_id" = "apps"."id" LEFT OUTER JOIN "users" ON "users"."id" = "lenses"."watcher_id"')
+      # .page params[:page]
     end
   end
 
